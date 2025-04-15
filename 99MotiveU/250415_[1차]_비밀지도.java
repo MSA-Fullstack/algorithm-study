c41d8d54611304a76ad91a4fa2d4b49cdaebd463
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-04-15 22:30:53 +0900

    feat: 99MotiveU 250415 1문제

---
+++ 99MotiveU/250415_[1차]_비밀지도.java
@@ -0,0 +1,24 @@
+class Solution {
+	public String[] solution(int n, int[] arr1, int[] arr2) {
+		String[] answer = {};
+		answer = new String[n]; 
+        
+		for(int i = 0; i < n; i++){
+			String binnum = Integer.toBinaryString(arr[i] | arr2[i]);
+            
+			while(binnum.length() < n){
+				binnum = "0" + binary;
+			}
+			String str = "";
+			for (int j = 0; j < n; j++){
+				if(binary.charAt(j) == '1'){
+					str += "#";
+				} else{
+					str += " ";
+				}
+			}
+			answer[i] = str;
+		}      
+		return answer;
+	}
+}
