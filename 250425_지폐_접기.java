fa664b4143597786db9356b9a0410e84eaff632c
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-04-25 22:41:26 +0900

    feat: 99MotiveU 250425 1문제

---
+++ 250425_지폐_접기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+
+        int walletW = Math.min(wallet[0], wallet[1]);
+        int walletH = Math.max(wallet[0], wallet[1]);
+
+        int billW = Math.min(bill[0], bill[1]);
+        int billH = Math.max(bill[0], bill[1]);
+
+        while (!((billW <= walletW && billH <= walletH) || (billH <= walletW && billW <= walletH))) {
+            boolean cutWidth = billW >= billH;
+            billW = cutWidth ? billW / 2 : billW;
+            billH = cutWidth ? billH : billH / 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
