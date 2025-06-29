f317f5c1989a4e8edd636b61525a30e3ab59e0f5
shong69 <shong7576@gmail.com>
2025-06-30 02:07:49 +0900

    feat: shong69 250630 3문제

---
+++ shong69/250628_의상.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0;i<clothes.length;i++){
+            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
+        }
+        for(Integer num:map.values()){
+            answer = answer*(num+1);
+        }
+        return answer-1;
+    }
+}
