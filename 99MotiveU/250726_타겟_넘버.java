a1469289ae6ab377d06d90c48c79e2c91a0aed33
Dong Ki Yoo <119654357+99MotiveU@users.noreply.github.com>
2025-09-22 00:03:50 +0900

    fix: 충돌 해결

--- 99MotiveU/250726_타겟_넘버.java
+++ 99MotiveU/250726_타겟_넘버.java
@@ -22,27 +22,3 @@ class Solution {
         return answer;
     }
 }
-=======
-class Solution {
-    public int solution(int[] numbers, int target) {
-        int answer = 0;  
-
-        java.util.Queue<int[]> q = new java.util.LinkedList<>();
-        q.offer(new int[]{0, 0});
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int idx = cur[0];
-            int sum = cur[1];
-
-            if (idx == numbers.length) {
-                if (sum == target) answer++;
-            } else {
-                q.offer(new int[]{idx + 1, sum + numbers[idx]});
-                q.offer(new int[]{idx + 1, sum - numbers[idx]});
-            }
-        }
-
-        return answer;
-    }
-}
